90a2d9e5b91d4cb8d5d73fe04c23f591627149f8
Azbabdesignhub <moshaaallah000@gmail.com>
2025-05-10 03:47:26 -0700

    Debugging gh-page error

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { BrowserRouter,Route, Routes} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +8,13 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App />
+
+    <BrowserRouter basename='/React_potfolio/'>
+      <Routes>
+        <Route path='*' element={<App />} />
+      </Routes>
+    </BrowserRouter>
+    
 
   </StrictMode>,
 )
